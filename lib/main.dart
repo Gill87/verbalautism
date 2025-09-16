9408b03977342f79f5718ab87dbd7bbadaf27bd2
Guritfak Gill <guritfak2007@gmail.com>
2025-09-16 15:16:11 -0700

    removed flutter tts entirely

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:verbalautism/components/audio%20services/tts_service.dart';
 import 'package:verbalautism/myapp.dart';
 import 'firebase_options.dart';
 
@@ -8,10 +7,6 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize TTS Service
-  final tts = TtsService();
-  await tts.init(); 
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
